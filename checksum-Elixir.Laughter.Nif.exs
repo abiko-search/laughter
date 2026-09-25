489f4fd76eb5d4c8a79bbbd6addbb00e8311ffe2
Danila Poyarkov <dev@dannote.net>
2026-09-26 00:27:38 +0400

    Update precompiled NIF checksums for v0.3.1

---
+++ checksum-Elixir.Laughter.Nif.exs
@@ -0,0 +1,7 @@
+%{
+  "liblaughter_nif-v0.3.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:6b8e468da1ab6938737a8642f294dfe4f08bd081e426bb2160aa3faabbcf68d7",
+  "liblaughter_nif-v0.3.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:2bb898d468becae3c6602089e358c6a878ee6de6151e46a7920252dd9fb44c8d",
+  "liblaughter_nif-v0.3.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:c9de793e0ef726db3a27906d449c914e8d83a5fc312225d3c576c3a6cdcf8446",
+  "liblaughter_nif-v0.3.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:05969e4971e4da82373c1363200e0740eda6c6f5cb48abd65902e917a7722ea6",
+  "liblaughter_nif-v0.3.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:cc875319648cb0872f99146edaefa8dc753d29960e3d8dd3ecb9afbf0840b507",
+}
